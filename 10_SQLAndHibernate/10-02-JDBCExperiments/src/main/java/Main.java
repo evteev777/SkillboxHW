12409ac065bf3bb42cbc3e2b3b2228373de96fb7
Alexander Evteev <9284108@mail.ru>
2020-03-23 21:19:03 +0300

    Correct log4j2 console output

--- 10_SQLAndHibernate/10-02-JDBCExperiments/src/main/java/Main.java
+++ 10_SQLAndHibernate/10-02-JDBCExperiments/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
@@ -14,8 +16,8 @@ public class Main {
     public static void main(String[] args) {
 
         Logger logger = LogManager.getRootLogger();
-        logger.info("Program start");
-        java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
+        Marker CONSOLE = MarkerManager.getMarker("Console");
+//        logger.warn(CONSOLE, "Program start");
 
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure("hibernate.cfg.xml")
@@ -29,10 +31,18 @@ public class Main {
 
         Session session = sessionFactory.openSession();
 
-        Course course = session.get(Course.class, 1);
-        logger.info(course.getName());
+        Course course1 = session.get(Course.class, 1);
+        Course course2 = session.get(Course.class, 2);
+        Course course3 = session.get(Course.class, 3);
+        Course course4 = session.get(Course.class, 4);
+        Course course5 = session.get(Course.class, 5);
+        logger.warn(CONSOLE, course1.getName());
+        logger.warn(CONSOLE, course2.getName());
+        logger.warn(CONSOLE, course3.getName());
+        logger.warn(CONSOLE, course4.getName());
+        logger.warn(CONSOLE, course5.getName());
 
         sessionFactory.close();
-        logger.info("Program completed");
+//        logger.warn(CONSOLE, "Program completed");
     }
 }

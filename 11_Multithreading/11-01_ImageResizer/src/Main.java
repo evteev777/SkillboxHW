79bd3d02510117c066470b081a7975d1fee87e96
Александр Евтеев <evteev777@gmail.com>
2020-05-23 07:45:34 +0300

    11.1 Image Resizer - split list of all files into parts, and start threads with these parts

--- 11_Multithreading/11-01_ImageResizer/src/Main.java
+++ 11_Multithreading/11-01_ImageResizer/src/Main.java
@@ -1,11 +1,11 @@
 
+import org.imgscalr.Scalr;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
 
 public class Main {
 
@@ -15,70 +15,117 @@ public class Main {
     private static final int PROCESSORS = Runtime.getRuntime().availableProcessors();
 
     public static void main(String[] args) throws InterruptedException {
-
-        convertAllImages(1);
-        convertAllImages(PROCESSORS);
+        convertAllImages(300, 1);
+        convertAllImages(300, PROCESSORS);
     }
 
-    private static void convertAllImages(int threadsCount) throws InterruptedException {
-        System.out.println("Threads count: " + threadsCount + "\n----------");
+    private static void convertAllImages(int newWidth, int threadsCount)
+            throws InterruptedException {
 
-        ExecutorService executorService = Executors.newFixedThreadPool(threadsCount);
-        File[] files = new File(SRC_FOLDER).listFiles();
-        long start = System.currentTimeMillis();
+        threadsCount = Math.min(threadsCount, PROCESSORS);
+        System.out.println("\nThreads count: " + threadsCount + "\n-----");
 
-        if (files != null) {
-            for (File file : files) {
-                executorService.submit(() -> {
+        File[] allFiles = new File(SRC_FOLDER).listFiles();
+        if (allFiles != null) {
+            Arrays.sort(allFiles, Comparator.comparing(File::length).reversed());
+            List<List<File>> fileLists = splitFileList(allFiles, threadsCount);
+
+            long start = System.currentTimeMillis();
+
+            startThreads(fileLists, threadsCount, newWidth);
+
+            System.out.println("Duration after all threads termination: " +
+                    (System.currentTimeMillis() - start) + " ms");
+        } else {
+            System.out.println(SRC_FOLDER + " is empty");
+        }
+    }
+
+    private static List<List<File>> splitFileList(File[] files, int partsCount) {
+        List<List<File>> fileLists = new ArrayList<>();
+
+        for (int i = 0; i < partsCount; i++) {
+            fileLists.add(new ArrayList<>());
+        }
+        int partNum = 0;
+        for (File file : files) {
+            fileLists.get(partNum).add(file);
+            partNum++;
+            if (partNum == partsCount)
+                partNum = 0;
+        }
+        return fileLists;
+    }
+
+    private static void startThreads(List<List<File>> fileLists, int threadsCount, int newWidth)
+            throws InterruptedException {
+
+        List<Thread> threads = new LinkedList<>();
+        for (int i = 0; i < threadsCount; i++) {
+            int threadNumber = i;
+            threads.add(new Thread(() -> {
+                List<File> fileList = fileLists.get(threadNumber);
+                for (File file : fileList) {
                     try {
-                        convertImage(file);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                        convertImage(file, newWidth);
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
                     }
-                });
-            }
+                }
+            }));
         }
 
-        System.out.println("Duration before shutdown: " + (System.currentTimeMillis() - start) + " ms");
+        for (Thread thread : threads) {
+            thread.start();
+        }
 
-        executorService.shutdown();
-        System.out.println("Duration after shutdown: " + (System.currentTimeMillis() - start) + " ms");
-
-        executorService.awaitTermination(1, TimeUnit.DAYS);
-        System.out.println("Duration after all threads termination: " +
-                (System.currentTimeMillis() - start) + " ms\n");
+        for (Thread thread : threads) {
+            thread.join();
+        }
     }
 
-    private static void convertImage(File file) throws IOException {
-
-        long convertStart = System.currentTimeMillis();
+    private static void convertImage(File file, int newWidth) throws IOException {
 
         BufferedImage image = ImageIO.read(file);
+        if (image != null) {
+            long convertStart = System.currentTimeMillis();
 
-        int newWidth = 300;
-        int newHeight = (int) Math.round(
-                image.getHeight() / (image.getWidth() / (double) newWidth)
-        );
-        BufferedImage newImage = new BufferedImage(
-                newWidth, newHeight, BufferedImage.TYPE_INT_RGB
-        );
+            BufferedImage newImage = scalrComverter(image, newWidth);
+//            BufferedImage newImage = habrArticleConverter(image, newWidth);
 
-        int widthStep = image.getWidth() / newWidth;
-        int heightStep = image.getHeight() / newHeight;
+            File newFile = new File(DST_FOLDER + "/" + formatFileName(file));
+            ImageIO.write(newImage, "jpg", newFile);
 
-        for (int x = 0; x < newWidth; x++) {
-            for (int y = 0; y < newHeight; y++) {
-                int rgb = image.getRGB(x * widthStep, y * heightStep);
-                newImage.setRGB(x, y, rgb);
-            }
+            long duration = System.currentTimeMillis() - convertStart;
+            print(file, image, newFile, newImage, duration);
+
         }
-
-        File newFile = new File(DST_FOLDER + "/" + formatFileName(file));
-        ImageIO.write(newImage, "jpg", newFile);
-
-        long duration = System.currentTimeMillis() - convertStart;
-        print(file, image, newFile, newImage, duration);
     }
+
+    private static BufferedImage scalrComverter(BufferedImage image, int imageSize) {
+        return Scalr.resize(image, Scalr.Method.ULTRA_QUALITY,
+                imageSize, Scalr.OP_ANTIALIAS);
+    }
+
+//    private static BufferedImage habrArticleConverter(BufferedImage image, int newWidth) {
+//        int newHeight = (int) Math.round(
+//                image.getHeight() / (image.getWidth() / (double) newWidth)
+//        );
+//        BufferedImage newImage = new BufferedImage(
+//                newWidth, newHeight, BufferedImage.TYPE_INT_RGB
+//        );
+//
+//        int widthStep = image.getWidth() / newWidth;
+//        int heightStep = image.getHeight() / newHeight;
+//
+//        for (int x = 0; x < newWidth; x++) {
+//            for (int y = 0; y < newHeight; y++) {
+//                int rgb = image.getRGB(x * widthStep, y * heightStep);
+//                newImage.setRGB(x, y, rgb);
+//            }
+//        }
+//        return newImage;
+//    }
 
     private static String formatFileName(File file) {
         String fileName = file.getName().substring(0, file.getName().lastIndexOf("."));

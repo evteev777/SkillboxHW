dfb6b9a1935e8704daa0fb87b500cf2100b34b3b
Александр Евтеев <evteev777@gmail.com>
2020-05-23 19:22:48 +0300

    11.1 Image Resizer - convert images: with Scalr library, and with Habr article method

--- 11_Multithreading/11-01_ImageResizer/src/Main.java
+++ 11_Multithreading/11-01_ImageResizer/src/Main.java
@@ -15,6 +15,7 @@ public class Main {
     private static final int PROCESSORS = Runtime.getRuntime().availableProcessors();
 
     public static void main(String[] args) throws InterruptedException {
+
         convertAllImages(300, 1);
         convertAllImages(300, PROCESSORS);
     }
@@ -26,59 +27,72 @@ public class Main {
         System.out.println("\nThreads count: " + threadsCount + "\n-----");
 
         File[] allFiles = new File(SRC_FOLDER).listFiles();
+
         if (allFiles != null) {
-            Arrays.sort(allFiles, Comparator.comparing(File::length).reversed());
-            List<List<File>> fileLists = splitFileList(allFiles, threadsCount);
+            // Sorting - for distribution files of different sizes by threads
+            Arrays.sort(allFiles, Comparator.comparing(File::length));
+
+            List<List<File>> files = splitAllFiles(allFiles, threadsCount);
 
             long start = System.currentTimeMillis();
 
-            startThreads(fileLists, threadsCount, newWidth);
+            convertFilesInThreads(files, threadsCount, newWidth);
 
             System.out.println("Duration after all threads termination: " +
                     (System.currentTimeMillis() - start) + " ms");
+
         } else {
-            System.out.println(SRC_FOLDER + " is empty");
+            System.out.println("Directory " + SRC_FOLDER + " is empty");
         }
     }
 
-    private static List<List<File>> splitFileList(File[] files, int partsCount) {
-        List<List<File>> fileLists = new ArrayList<>();
+    private static List<List<File>> splitAllFiles(File[] files, int partsCount) {
+
+        List<List<File>> lists = new ArrayList<>();
 
         for (int i = 0; i < partsCount; i++) {
-            fileLists.add(new ArrayList<>());
+            lists.add(new ArrayList<>());
         }
-        int partNum = 0;
+
+        int partNumber = 0;
+
         for (File file : files) {
-            fileLists.get(partNum).add(file);
-            partNum++;
-            if (partNum == partsCount)
-                partNum = 0;
+            lists.get(partNumber).add(file);
+
+            partNumber++;
+            if (partNumber == partsCount)
+                partNumber = 0;
         }
-        return fileLists;
+        return lists;
     }
 
-    private static void startThreads(List<List<File>> fileLists, int threadsCount, int newWidth)
+    private static void convertFilesInThreads(
+            List<List<File>> files, int threadsCount, int newWidth)
             throws InterruptedException {
 
         List<Thread> threads = new LinkedList<>();
+
         for (int i = 0; i < threadsCount; i++) {
-            int threadNumber = i;
+            int finalI = i;
+
             threads.add(new Thread(() -> {
-                List<File> fileList = fileLists.get(threadNumber);
-                for (File file : fileList) {
+
+                List<File> list = files.get(finalI);
+
+                for (File file : list) {
+
                     try {
                         convertImage(file, newWidth);
+
                     } catch (IOException ioException) {
                         ioException.printStackTrace();
                     }
                 }
             }));
         }
-
         for (Thread thread : threads) {
             thread.start();
         }
-
         for (Thread thread : threads) {
             thread.join();
         }
@@ -87,56 +101,81 @@ public class Main {
     private static void convertImage(File file, int newWidth) throws IOException {
 
         BufferedImage image = ImageIO.read(file);
+
         if (image != null) {
-            long convertStart = System.currentTimeMillis();
 
-            BufferedImage newImage = scalrComverter(image, newWidth);
-//            BufferedImage newImage = habrArticleConverter(image, newWidth);
+            BufferedImage newImageScalr = comverterScalr(image, newWidth);
+            File newFileScalr = new File(DST_FOLDER + "/S-" + formatFileName(file));
+            ImageIO.write(newImageScalr, "jpg", newFileScalr);
 
-            File newFile = new File(DST_FOLDER + "/" + formatFileName(file));
-            ImageIO.write(newImage, "jpg", newFile);
+            BufferedImage newImageHabr = converterHabr(image, newWidth);
+            File newFileHabr = new File(DST_FOLDER + "/H-" + formatFileName(file));
+            ImageIO.write(newImageHabr, "jpg", newFileHabr);
 
-            long duration = System.currentTimeMillis() - convertStart;
-            print(file, image, newFile, newImage, duration);
-
+            print(file, image, newFileScalr, newImageScalr);
+            print(file, image, newFileHabr, newImageHabr);
         }
     }
 
-    private static BufferedImage scalrComverter(BufferedImage image, int imageSize) {
-        return Scalr.resize(image, Scalr.Method.ULTRA_QUALITY,
-                imageSize, Scalr.OP_ANTIALIAS);
+    private static BufferedImage comverterScalr(BufferedImage image, int imageSize) {
+        return Scalr.resize(image, Scalr.Method.ULTRA_QUALITY, imageSize, Scalr.OP_ANTIALIAS);
     }
 
-//    private static BufferedImage habrArticleConverter(BufferedImage image, int newWidth) {
-//        int newHeight = (int) Math.round(
-//                image.getHeight() / (image.getWidth() / (double) newWidth)
-//        );
-//        BufferedImage newImage = new BufferedImage(
-//                newWidth, newHeight, BufferedImage.TYPE_INT_RGB
-//        );
-//
-//        int widthStep = image.getWidth() / newWidth;
-//        int heightStep = image.getHeight() / newHeight;
-//
-//        for (int x = 0; x < newWidth; x++) {
-//            for (int y = 0; y < newHeight; y++) {
-//                int rgb = image.getRGB(x * widthStep, y * heightStep);
-//                newImage.setRGB(x, y, rgb);
-//            }
-//        }
-//        return newImage;
-//    }
+    private static BufferedImage converterHabr(BufferedImage image, int newWidth) {
+
+        int scale = 0;
+
+        if (image.getWidth() != newWidth) {
+
+            int width = image.getWidth();
+
+            while (width / newWidth > 1) {
+                width /= 2;
+                scale++;
+            }
+
+            image = resizeHabr(image, newWidth, scale);
+        }
+
+        for (int i = 0; i < scale; i++) {
+            image = resizeHabr(image, newWidth, 0);
+        }
+
+        return image;
+    }
+
+    private static BufferedImage resizeHabr(BufferedImage image, int newWidth, int scale) {
+
+        int width = newWidth * (int) Math.pow(2, scale);
+        int height = (int) Math.round((double) width / image.getWidth() * image.getHeight());
+
+        BufferedImage resizedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        double widthStep = (double) image.getWidth() / width;
+        double heightStep = (double) image.getHeight() / height;
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int rgb = image.getRGB((int) (x * widthStep), (int) (y * heightStep));
+                resizedImage.setRGB(x, y, rgb);
+            }
+        }
+
+        return resizedImage;
+    }
 
     private static String formatFileName(File file) {
         String fileName = file.getName().substring(0, file.getName().lastIndexOf("."));
         return (fileName.length() <= 8 ? fileName : fileName.substring(0, 8) + "~") + ".jpg";
     }
 
-    private static void print(File file, BufferedImage image, File newFile, BufferedImage newImage, long duration) {
-        System.out.printf("Converted:\tThread %s\t%-13s\t%6s kb\t%s x %s px\t-> \t%-12s\t%6s kb\t%s x %s px\t%3s ms%n",
+    private static void print(File file, BufferedImage image, File newFile, BufferedImage newImage) {
+        System.out.printf(
+                "Converted:\tThread %s\t%-13s\t%6s kb\t%s x %s px\t-> \t%-12s\t%6s kb\t%s x %s px%n",
                 Thread.currentThread().getId(),
-                formatFileName(file), file.length() / 1024, image.getWidth(), image.getHeight(),
-                formatFileName(newFile), newFile.length() / 1024, newImage.getWidth(), newImage.getHeight(),
-                duration);
+                formatFileName(file), file.length() / 1024,
+                image.getWidth(), image.getHeight(),
+                formatFileName(newFile), newFile.length() / 1024,
+                newImage.getWidth(), newImage.getHeight());
     }
 }
